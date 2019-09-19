795ddd5116c5adbee6f917767d39aefb0c59f3cd
mudimuteba <mudi.muteba@gmail.com>
2019-09-19 12:49:56 +0200

    optimized regex and refactored

--- string_calculator.py
+++ string_calculator.py
@@ -3,12 +3,10 @@ import re
 def add(numbers):
     if numbers == '': return 0
 
-    numbers = re.findall(r"-\d+|\d+", numbers)
-    numbers = list(map(int, numbers))
+    numbers = map(int, re.findall(r"-?\d+", numbers))
+    numbers = filter(lambda x: x < 1000, numbers)
     negative_numbers = filter(lambda x: x < 0, numbers)
 
     if negative_numbers: raise Exception('negatives not allowed ' + str(negative_numbers))
-
-    numbers = filter(lambda x: x >= 0 and x < 1000, numbers)
 
     return sum(numbers)
